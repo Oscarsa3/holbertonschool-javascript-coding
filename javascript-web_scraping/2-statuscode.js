207b6974a2900ed91b7d57dcd4743e6bc204ad2a
Oscarsa3 <oscarsadev@gmail.com>
2023-09-24 22:40:18 +0000

    fix: semistandard

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,8 @@ const url = process.argv[2];
 const request = require('request');
 
 request(url, (error, response) => {
-  if (error) { console.error(error) };
+  if (error) {
+    console.error(error);
+  };
   console.log('code: ' + response.statusCode);
 });
