6d620c00ac1c04565974ee0f5850b3ecff02f83f
Oscarsa3 <oscarsadev@gmail.com>
2023-09-25 00:57:20 +0000

    feat: add a script that computes the number of tasks completed by user id.

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+
+request(url, (error, response, body) => {
+  const alls = JSON.parse(body);
+  const results = {};
+  for (all of alls) {
+    const id = all.userId.toString();
+    if (!results[id]) {
+      results[id] = 0;
+    }
+    if (all.completed) {
+      results[id]++;
+    }
+  }
+  for (const k in results) {
+    if (!results[k]){
+      delete result[k];
+    }
+  }
+  console.log(results);
+});
