b10a0d93f92deef9671d853d811e3421dd5658d7
Oscarsa3 <oscarsadev@gmail.com>
2023-09-25 01:05:04 +0000

    fix: semistandard

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,9 +4,12 @@ const url = process.argv[2];
 const request = require('request');
 
 request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
   const alls = JSON.parse(body);
   const results = {};
-  for (all of alls) {
+  for (const all of alls) {
     const id = all.userId.toString();
     if (!results[id]) {
       results[id] = 0;
