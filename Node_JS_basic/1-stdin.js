3d7631016b7c160a78d5f5563766d2f68aa1a860
Oscarsa3 <oscarsadev@gmail.com>
2023-10-12 13:41:26 -0500

    fix: fix output

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', data => { 
-    console.log(`Your name is: ${data}`);
-})
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+  process.exit();
+});
 
 process.on('exit', () => {
-    console.log('This important software is now closing');
-    process.exit();
-})
+  process.stdout.write('This important software is now closing');
+  process.exit();
+});
