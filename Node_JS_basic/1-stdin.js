f8766961b2c89412ad6048d52788389f636ea64b
Oscarsa3 <oscarsadev@gmail.com>
2023-10-12 13:45:34 -0500

    fix: fix line break

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,8 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
-  process.exit();
 });
 
 process.on('exit', () => {
-  process.stdout.write('This important software is now closing');
-  process.exit();
+  console.log('This important software is now closing \n');
 });
