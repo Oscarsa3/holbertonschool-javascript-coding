2dc0318b52a2096919f7a3c60acfbb6f2662653e
Oscarsa3 <oscarsadev@gmail.com>
2023-10-13 22:45:45 -0500

    fix: update

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -9,14 +9,12 @@ function countStudents(path) {
     lines.forEach((line) => {
       getField[line[line.length - 1]] = getField[line[line.length - 1]] + 1 || 1;
     });
-
     console.log(`Number of students: ${lines.length}`);
 
     for (const field in getField) {
       if (field) {
         const names = lines.filter((line) => line[line.length - 1] === field)
           .map((name) => name[0]);
-
         console.log(`Number of students in ${field}: ${getField[field]}. List: ${names.join(', ')}`);
       }
     }
