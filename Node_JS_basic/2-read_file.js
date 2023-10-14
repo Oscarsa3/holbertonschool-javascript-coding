c618fbd58867808f34cbaa8f3a83221a8d7c98f9
Oscarsa3 <oscarsadev@gmail.com>
2023-10-13 22:24:39 -0500

    feat: using csv file

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    let lines = data.split('\n').map((line) => line.split(',').map((field) => field.replace('\r', '')));
+    lines = lines.slice(1, lines.length);
+    const getField = {};
+    lines.forEach((line) => {
+      getField[line[line.length - 1]] = getField[line[line.length - 1]] + 1 || 1;
+    });
+
+    console.log(`Number of students: ${lines.length}`);
+
+    for (const field in getField) {
+      if (field) {
+        const names = lines.filter((line) => line[line.length - 1] === field)
+          .map((name) => name[0]);
+
+        console.log(`Number of students in ${field}: ${getField[field]}. List: ${names.join(', ')}`);
+      }
+    }
+  } catch (error) {
+    console.error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
