0e810b471b4e1f830e6eca8e0ee75fd653598421
Oscarsa3 <oscarsadev@gmail.com>
2023-10-14 20:20:56 -0500

    fix: trow error

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -2,28 +2,32 @@ const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      try {
-        let lines = data.split('\n').map((line) => line.split(',').map((field) => field.trim().replace('\r', '')));
-        lines = lines.slice(1, lines.length);
-        const getField = {};
-        lines.forEach((line) => {
-          getField[line[line.length - 1]] = getField[line[line.length - 1]] + 1 || 1;
-        });
-        console.log(`Number of students: ${lines.length}`);
+    if (fs.existsSync(path)) {
+      fs.readFile(path, 'utf8', (error, data) => {
+        if (error) {
+          console.log(error);
+        } else {
+          let lines = data.split('\n').map((line) => line.split(',').map((field) => field.trim().replace('\r', '')));
+          lines = lines.slice(1, lines.length);
+          const getField = {};
+          lines.forEach((line) => {
+            getField[line[line.length - 1]] = getField[line[line.length - 1]] + 1 || 1;
+          });
+          console.log(`Number of students: ${lines.length}`);
 
-        for (const field in getField) {
-          if (field) {
-            const names = lines.filter((line) => line[line.length - 1] === field)
-              .map((name) => name[0]);
-            console.log(`Number of students in ${field}: ${getField[field]}. List: ${names.join(', ')}`);
+          for (const field in getField) {
+            if (field) {
+              const names = lines.filter((line) => line[line.length - 1] === field)
+                .map((name) => name[0]);
+              console.log(`Number of students in ${field}: ${getField[field]}. List: ${names.join(', ')}`);
+            }
           }
+          resolve(lines);
         }
-        resolve();
-      } catch (error) {
-        throw new Error('Cannot load the database');
-      }
-    });
+      });
+    } else {
+      throw new Error('Cannot load the database');
+    }
   });
 }
 
