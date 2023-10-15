114e64b5f95381fdd2a20748fbaec8425c94c9d8
Oscarsa3 <oscarsadev@gmail.com>
2023-10-14 20:12:38 -0500

    fix: trow error

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -3,25 +3,26 @@ const fs = require('fs');
 function countStudents(path) {
   return new Promise((resolve) => {
     fs.readFile(path, 'utf8', (error, data) => {
-      if (error) {
+      try {
+        let lines = data.split('\n').map((line) => line.split(',').map((field) => field.trim().replace('\r', '')));
+        lines = lines.slice(1, lines.length);
+        const getField = {};
+        lines.forEach((line) => {
+          getField[line[line.length - 1]] = getField[line[line.length - 1]] + 1 || 1;
+        });
+        console.log(`Number of students: ${lines.length}`);
+
+        for (const field in getField) {
+          if (field) {
+            const names = lines.filter((line) => line[line.length - 1] === field)
+              .map((name) => name[0]);
+            console.log(`Number of students in ${field}: ${getField[field]}. List: ${names.join(', ')}`);
+          }
+        }
+        resolve();
+      } catch (error) {
         throw new Error('Cannot load the database');
       }
-      let lines = data.split('\n').map((line) => line.split(','));
-      lines = lines.slice(1, lines.length);
-      const getField = {};
-      lines.forEach((line) => {
-        getField[line[line.length - 1]] = getField[line[line.length - 1]] + 1 || 1;
-      });
-      console.log(`Number of students: ${lines.length}`);
-
-      for (const field in getField) {
-        if (field) {
-          const names = lines.filter((line) => line[line.length - 1] === field)
-            .map((name) => name[0]);
-          console.log(`Number of students in ${field}: ${getField[field]}. List: ${names.join(', ')}`);
-        }
-      }
-      resolve();
     });
   });
 }
