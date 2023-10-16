ce3011d8fc5d6e1bfc8ac3582c4316cf5f399b7e
Oscarsa3 <oscarsadev@gmail.com>
2023-10-15 22:10:57 -0500

    feat: routes

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.end(data);
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  }
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
