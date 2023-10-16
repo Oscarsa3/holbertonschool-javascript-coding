1d47a81b90f54f147f5eb3e5e55663839a3d5235
Oscarsa3 <oscarsadev@gmail.com>
2023-10-15 23:59:07 -0500

    feat: full server

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,59 @@
+import readDatabase from '../utils';
+const fs = require('fs');
+const db = process.argv[2];
+
+class StudentsController {
+    static getAllStudents(req, res) {
+        res.status(200);
+        if (fs.existsSync(db)) {
+          readDatabase(db)
+            .then((data) => {
+              const students = [];
+  
+              res.write('This is the list of our students\n');
+  
+              for (const field in data) {
+                res.write(`Number of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}\n`);
+              }
+              res.end();
+            })
+            .catch((err) => {
+              res.end(err.message);
+            })
+        } else {
+            res.status(500);
+            res.write('Cannot load the database');
+        }
+    }
+
+    static getAllStudentsByMajor(req, res) {
+        
+        res.status(200);
+
+        if (fs.existsSync(db)) {
+            readDatabase(db)
+              .then((data) => {
+                const major = req.params.major;
+                if (data[major]) {
+                  res.status(200);
+                  res.write(`List: ${data[major].join(', ')}`);
+                } else {
+                  res.status(500);
+                  res.setHeader('Content-Type', 'text/plain');
+                  res.end('Major parameter must be CS or SWE');
+                }
+                res.end();
+              })
+              .catch((err) => {
+                res.status(500);
+                res.end(err.message);
+              })
+        } else {
+            res.status(500);
+            res.setHeader('Content-Type', 'text/plain');
+            res.write('Cannot load the database');
+        }
+    }
+}
+
+module.exports = StudentsController;
